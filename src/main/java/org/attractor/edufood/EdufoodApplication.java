1fa6118333ae6fbcec028409e8b887e53de752fa
Beksultan Tatenov <bekatatenov@gmail.com>
2021-03-08 19:26:01 +0600

    Initial commit

---
+++ src/main/java/org/attractor/edufood/EdufoodApplication.java
@@ -0,0 +1,13 @@
+package org.attractor.edufood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdufoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdufoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/attractor/edufood/EdufoodApplicationTests.java
@@ -0,0 +1,13 @@
+package org.attractor.edufood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdufoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
